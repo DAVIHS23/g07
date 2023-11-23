3bd4a2d51d8e0ad52604488037ea7f1d688b22af
Vithursiya <vithursiya.vijayasingam@stud.hslu.ch>
2023-11-23 14:47:29 +0100

    bar add csv file

--- js/bar.js
+++ js/bar.js
@@ -31,7 +31,7 @@ var yAxis = svg.append("g")
 function update(selectedVar) {
 
   // Parse the Data
-  d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/barplot_change_data.csv", function(data) {
+  d3.csv("https://raw.githubusercontent.com/DAVIHS23/g07/main/data/IQ_level.csv", function(data) {
 
     // X axis
     x.domain(data.map(function(d) { return d.group; }))
