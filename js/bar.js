cba3274cade97f94d67a11fadb08852079eb263a
Vithursiya <vithursiya.vijayasingam@stud.hslu.ch>
2023-11-24 14:02:07 +0100

    responsive bar

--- js/bar.js
+++ js/bar.js
@@ -1,84 +1,103 @@
-var margin = {top: 20, right: 30, bottom: 40, left: 90},
-    width = 1000 - margin.left - margin.right,
-    height = 1000 - margin.top - margin.bottom;
+var margin = { top: 20, right: 30, bottom: 40, left: 90 },
+  width = 1000 - margin.left - margin.right,
+  height = 1000 - margin.top - margin.bottom;
 
-var svgBar = d3.select("#barChart")
+// Define scales and axes
+var x = d3.scaleLinear().range([0, width]);
+var y = d3.scaleBand().range([height, 0]).padding(0.1);
+
+var xAxis = d3.axisBottom(x);
+var yAxis = d3.axisLeft(y);
+
+var svgBar = d3
+  .select("#barChart")
   .append("svg")
-    .attr("width", "100%")
-    .attr("height", "100%")
+  .attr("width", "100%")
+  .attr("height", "100%")
   .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json("https://raw.githubusercontent.com/DAVIHS23/g07/main/data/IQ_level.json", function(data) {
-
-  // data.sort(function(a, b) {
-  //   return a.IQ - b.IQ;
-  // });
-  
+d3.json("https://raw.githubusercontent.com/DAVIHS23/g07/main/data/IQ_level.json", function (data) {
   var top20Data = data.slice(0, 20);
+  top20Data.reverse();
 
-  top20Data.reverse()
+  x.domain([0, d3.max(top20Data, function (d) { return d.IQ; })]);
+  y.domain(top20Data.map(function (d) { return d.country; }));
 
-  // Add Y axis
-  var y = d3.scaleBand()
-    .range([height, 0])
-    .domain(top20Data.map(function (d) { return d.country; }))
-    .padding(0.1);
-  svgBar.append("g")
-    .call(d3.axisLeft(y))
+  svgBar.append("g").attr("class", "y-axis").call(d3.axisLeft(y));
 
-  // X axis
-  var x = d3.scaleLinear()
-    .domain([0, d3.max(top20Data, function (d) { return d.IQ; })])
-    .range([0, width]);
-  svgBar.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
+  svgBar.append("g").attr("class", "x-axis").attr("transform", "translate(0," + height + ")").call(xAxis);
 
-    let mouseOver = function (d) {
-      svgBar.selectAll("rect")
-        .transition()
-        .style("opacity", 0.5);
-  
-      d3.select(this)
-        .transition()
-        .style("opacity", 1)
-
-      var tooltipContent = `
-        <strong>${d.country}</strong><br>
-        Rank: ${d.rank}<br>
-        IQ: ${d.IQ}<br>
-      `;
-
-      tooltip.html(tooltipContent)
-        .style("left", (d3.event.pageX + 10) + "px")
-        .style("top", (d3.event.pageY - 30) + "px")
-        .style("opacity", 1);
-      
-    };
-  
-    let mouseLeave = function (d) {
-      svgBar.selectAll("rect")
-        .transition()
-        .style("opacity", 0.8);
-  
-      d3.select(this)
-        .transition()
-  
-      tooltip.style("opacity", 0);
-      
-    };
-
-  svgBar.selectAll("myRect")
+  svgBar
+    .selectAll(".myRect")
     .data(top20Data)
     .enter()
     .append("rect")
+    .attr("class", "myRect") 
     .attr("x", 0)
-    .attr("y", function (d) { return y(d.country); })
-    .attr("width", function (d) { return x(d.IQ); })
+    .attr("y", function (d) {
+      return y(d.country);
+    })
+    .attr("width", function (d) {
+      return x(d.IQ);
+    })
     .attr("height", y.bandwidth())
     .attr("fill", "#69b3a2")
     .on("mouseover", mouseOver)
-    .on("mouseleave", mouseLeave)
+    .on("mouseleave", mouseLeave);
+
+  function mouseOver(d) {
+    svgBar.selectAll(".myRect") 
+      .transition()
+      .style("opacity", 0.5);
+
+    d3.select(this)
+      .transition()
+      .style("opacity", 1);
+
+    var tooltipContent = `
+      <strong>${d.country}</strong><br>
+      Rank: ${d.rank}<br>
+      IQ: ${d.IQ}<br>
+    `;
+
+    tooltip
+      .html(tooltipContent)
+      .style("left", d3.event.pageX + 10 + "px")
+      .style("top", d3.event.pageY - 30 + "px")
+      .style("opacity", 1);
+  }
+
+  function mouseLeave(d) {
+    svgBar.selectAll(".myRect")
+      .transition()
+      .style("opacity", 0.8);
+
+    d3.select(this)
+      .transition();
+
+    tooltip.style("opacity", 0);
+  }
 });
+
+function update(selectedVar) {
+  d3.json("https://raw.githubusercontent.com/DAVIHS23/g07/main/data/IQ_level.json", function (data) {
+    var top20Data = data.slice(0, 20);
+    top20Data.reverse();
+
+    x.domain([0, d3.max(top20Data, function (d) { return +d[selectedVar]; })]);
+    y.domain(top20Data.map(function (d) { return d.country; }));
+
+    svgBar.select(".x-axis").transition().duration(1000).call(xAxis);
+
+    svgBar.select(".y-axis").transition().duration(1000).call(yAxis);
+
+    var u = svgBar.selectAll(".myRect").data(top20Data);
+
+    u.enter().append("rect").merge(u).transition().duration(1000).attr("x", 0).attr("y", function (d) {
+      return y(d.country);
+    }).attr("width", function (d) {
+      return x(+d[selectedVar]);
+    }).attr("height", y.bandwidth()).attr("fill", "#69b3a2");
+  });
+}
--- js/map.js
+++ js/map.js
@@ -1,25 +1,20 @@
 
 var worldData;
 
-// The svg
 var svg = d3.select("#iq_map"),
   width = +svg.attr("width"),
   height = +svg.attr("height");
-
-// Map and projection
 var path = d3.geoPath();
 var projection = d3.geoMercator()
   .scale(100)
   .center([0, 20])
   .translate([width / 2, height / 2]);
 
-// Data and color scale
 var data = d3.map();
 var colorScale = d3.scaleThreshold()
   .domain([50, 60, 70, 80, 90, 95, 100, 105])  // Adjust as needed
   .range(d3.schemeGreens[8]); 
 
-// Load external data and boot
 d3.queue()
   .defer(d3.json, "https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json")
   .defer(d3.json, "https://raw.githubusercontent.com/DAVIHS23/g07/main/data/IQ_level.json")
@@ -27,6 +22,7 @@ d3.queue()
 
 
 function ready(error, topo, IQLevelData) {
+  
   worldData = topo
   worldData.features = worldData.features.filter(function (d) {
     return d.properties.name !== "Antarctica";
@@ -45,7 +41,6 @@ function ready(error, topo, IQLevelData) {
       .style("stroke", "black");
 
     var iqData = IQLevelData.find((iq) => iq.country === d.properties.name);
-    console.log(d.properties.name)
 
     if (iqData) {
       var tooltipContent = `
@@ -96,7 +91,7 @@ function ready(error, topo, IQLevelData) {
     .attr("d", d3.geoPath().projection(projection))
     .attr("fill", function (d) {
       var iqData = IQLevelData.find((iq) => iq.country === d.properties.name);
-      return iqData ? colorScale(iqData.IQ) : "gray";  // Default to gray if no IQ data is available
+      return iqData ? colorScale(iqData.IQ) : "gray";
     })
     .style("stroke", "transparent")
     .attr("class", function (d) { return "Country" })
@@ -106,7 +101,6 @@ function ready(error, topo, IQLevelData) {
   
 }
 
-// Define a tooltip div
 var tooltip = d3.select("body").append("div")
   .attr("class", "tooltip")
   .style("opacity", 0);
